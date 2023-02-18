e6a21bf8018180f525611e7f921cf8be53b77be4
eundeang <2021112027@dgu.ac.kr>
2023-02-18 15:11:59 +0900

    updown 게임 UI만 완성

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    var comNumber = Int.random(in: 1...10)
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // 1) 메인레이블에 " 선택하세요""
+        // 2) 숫자 레이블은 ""
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        // 1) 버튼의 숫자를 가져와
+        // 2) 숫자 레이블이 변하도록 (숫자에 따라)
+        // 3) 선택한 숫자를 변수에다가 저장 (선택)
+    }
+    
+    @IBAction func selectButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+    
 }
 
